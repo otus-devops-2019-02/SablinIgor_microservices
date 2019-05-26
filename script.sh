9315dfc596921124790a921878938fdb3b8ec41a
SablinIgor <sablin.igor@gmail.com>
2019-05-26 17:04:42 +0300

    Create target group

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ aws configure set region eu-west-3
 
 apt-get install jq -y
 
-#eval TARGET_GROUP_ARN=$(aws elbv2 create-target-group --name Test-tg-commit3 --protocol HTTP --port 80 --vpc-id vpc-060e5cc464cd74d72 | jq -c '.TargetGroups[0].TargetGroupArn')
+eval TARGET_GROUP_ARN=$(aws elbv2 create-target-group --name Test-tg-$CI_ENVIRONMENT_SLUG --protocol HTTP --port 80 --vpc-id vpc-060e5cc464cd74d72 | jq -c '.TargetGroups[0].TargetGroupArn')
 
 # create load balancer: get LOAD_BALANCER_ARN and DNS_NAME
 #read LOAD_BALANCER_ARN DNS_NAME <<< $(aws elbv2 create-load-balancer --name App-LB-Balancer-commit3 --type application --subnets subnet-05349e5e055408210 subnet-06357c0e76a089427 | jq -r '.LoadBalancers[0] | "\(.LoadBalancerArn) \(.DNSName)"')
